306f877f37180d3ffc5af26dd6f33cc0257b7211
murpheycm <cmmurphey@gmail.com>
2023-09-24 16:44:25 -0400

    Updated models folder and setup index.js for models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Username = require('./username');
+const Email = require('./email');
+const Thoughts = require('./thoughts');
+const Friends = require('./friends');
+
+module.exports = { Username, Email, Thoughts, Friends };
