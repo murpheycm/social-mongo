bdbce9d1c55706c9987b46ea06a4a1f6d27855cd
murpheycm <cmmurphey@gmail.com>
2023-09-24 18:14:10 -0400

    Working on controllers

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,31 @@
+// Imports
+const { User, Thoughts } = require('../models');
+
+// ===================Comments==============//
+// GET all comments
+const thoughtsController = {
+
+// GET individual comment
+
+
+
+// CREATE comment
+
+
+
+// UPDATE comment
+
+// DELETE comment
+
+
+// =================Reactions================//
+// Add reaction
+
+
+// Remove reaction
+
+
+};
+
+// Exports
+module.exports = thoughtsController;
---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+// Imports
+const { User, Thoughts } = require('../models');
+
+// GET all users
+const userController = {
+
+
+// GET individual user
+
+
+
+// CREATE user
+
+
+// UPDATE user
+
+
+
+// DELETE user
+
+};
+
+// Export
+module.exports = userController;
