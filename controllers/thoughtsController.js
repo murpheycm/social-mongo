0e0087da859da9921eb1a11b0b334e3feca2a78f
murpheycm <cmmurphey@gmail.com>
2023-10-18 21:51:29 -0400

    Updated thoughtsController for update and delete thoughts

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -2,7 +2,6 @@
 const { User, Thoughts } = require('../models');
 
 // ===================Comments==============//
-
 const thoughtsController = {
 // GET all comments
     async getThoughts(req, res) {
@@ -31,15 +30,66 @@ const thoughtsController = {
         }
     },
 
-
 // CREATE comment
+    async createThought(req, res) {
+        try {
+            const thought = await Thoughts.create(req.body);
+            const user = await User.findByIdAndUpdate(
+                req.body.userId,
+                { $addToSet: { thoughts: thought._id }},
+                { 
+                    new: true,
+                    runValidators: true 
+                },
+            );
 
-
+            return res.status(200).json({ thought, user });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 // UPDATE comment
+    async updateThought(req, res) {
+        try {
+            const thought = await Thoughts.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { 
+                    new: true,
+                    runValidators: true 
+                }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: "Please check the ID you provided" });
+            }
+
+            return res.status(200).json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 // DELETE comment
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thoughts.findOneAndDelete(
+                {_id: req.params.thoughtId}
+            );
 
+            if (!thought) {
+                return res.status(404).json({ message: "Please check the ID you provided" });
+            }
+
+            return res.status(200).json({message: "Thought deleted"});
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 // =================Reactions================//
 // Add reaction
