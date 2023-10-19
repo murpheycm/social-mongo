39748d4c685021516d34d00631f82b7c0c0d75c5
murpheycm <cmmurphey@gmail.com>
2023-10-18 21:39:21 -0400

    Added GET code for both all and single thoughts

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,12 +1,35 @@
-// Imports
+// Imports User and THoughts Models
 const { User, Thoughts } = require('../models');
 
 // ===================Comments==============//
-// GET all comments
+
 const thoughtsController = {
+// GET all comments
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thoughts.find();
+            return res.status(200).json(thoughts);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 // GET individual comment
+    async getThought(req, res) {
+        try {
+            const thought = await Thoughts.findOne({ _id: req.params.thoughtId });
 
+            if (!thought) {
+                return res.status(404).json({ message: "Please check the ID you provided" });
+            }
+
+            return res.status(200).json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 
 // CREATE comment
