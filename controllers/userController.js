78144f47adc643d63986fa4aa87b3790e50bb08f
murpheycm <cmmurphey@gmail.com>
2023-10-18 21:20:56 -0400

    Updated userController with code to create a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,9 +36,16 @@ const userController = {
         }
     },
 
-
 // CREATE user
-
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            return res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 // UPDATE user
 
